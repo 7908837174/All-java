1076c64ddafb8036a50cf25400016ecd9ab83a97
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-15 04:29:03 +0530

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,18 @@
+package com.example.kallalkoti
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_screen)
+
+    }
+}
