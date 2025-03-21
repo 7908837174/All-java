d2ffa7deb2f5b3f6592a07a16710e655e83802aa
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-22 03:13:23 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,25 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(2+3);
+        boolean isPassed =true;
+        int count=24;
+        long countLong=count;
+        byte marks=25;
+        float kallal=3.14f;
+        double secondPi=3.14;
+        System.out.println(count);
+        System.out.println(marks);
+        System.out.println(kallal);
+        System.out.println(secondPi);
+        char mylater='k';
+        int age=23;
+        byte newAge=(byte)age;// loss data
+        int a=23;
+        int b=45;
+        double c=(double)a/b;
+        System.out.println(c);
+        // write basic sintex of java;
+        
+        
+    }
+}
