c2187e938e73285092bbd2d3638293e6ede269b3
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-22 03:11:45 +0530

    Create ath1.java

---
+++ ath1.java
@@ -0,0 +1,15 @@
+// public class math33 {
+//     public static void main(String[] args) {
+//         double abs=Math.random();
+//         double a1=Math.abs(-5,4);
+//         System.out.println(abs);
+//     }
+// }
+ public class Math3{
+    public static void main(String[] args) {
+        double randomValue = Math.random();
+        double absoluteValue = Math.abs(-5.4); 
+        System.out.println("Random Value: " + randomValue);
+        System.out.println("Absolute Value: " + absoluteValue);
+    }
+ }
