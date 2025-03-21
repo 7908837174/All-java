aba22ab5b18dc943a43acb3b6ede130b823ec095
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-22 03:37:51 +0530

    Create Pst.java

---
+++ Pst.java
@@ -0,0 +1,128 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+// import college.Book;
+// import college.Mentor;
+// import college.Student;
+
+// public class PST {
+//    public PST() {
+//    }
+
+//    public static void main(String[] var0) {
+//       Book var1 = new Book("Kallal", 267);
+//       Student var2 = new Student("kallal", 18, 6);
+//       Mentor var3 = new Mentor("Kako", 27, 12);
+//       System.out.println(var1.name + ": " + var1.price);
+//       System.out.println(var2.name + " " + var2.age + " " + var2.rollNo);
+//       System.out.println(var3.name + " " + var3.age + " " + var3.employId);
+//    }
+// }
+
+
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.time.LocalTime;
+// import java.time.format.DateTimeFormatter;
+
+// public class Date55 {
+//     public static void main(String[] args) {
+//         LocalDate d=LocalDate.now();
+//         LocalDate e=LocalDate.of(7474, 12, 4);
+//         System.out.println(e);
+//         System.out.println(d);
+//         LocalTime w=LocalTime.now();
+//         System.out.println(w);
+//         System.out.println("Month" + myDate.getDayOfMonth());
+        
+//     }
+// }
+// import java.time.LocalDate;
+// import java.time.LocalTime;
+
+// public class Date55 {
+//     public static void main(String[] args) {
+//         LocalDate today = LocalDate.now();
+    
+//         LocalDate specificDate = LocalDate.of(7474, 12, 4);
+    
+//         System.out.println("Specific Date: " + specificDate);
+//         System.out.println("Current Date: " + today);
+    
+//         LocalTime currentTime = LocalTime.now();
+//         System.out.println("Current Time: " + currentTime);
+//         System.out.println("Day of the Month: " + today.getDayOfMonth());
+//         LocalDate current_Date=LocalDate.now();
+//         LocalDate myDate=LocalDate.of(2344, 02, 12);
+
+//         boolean res=myDate.isBefore( current_Date);
+//         System.out.println(res); 
+
+//         import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
+    // public static void main(String[] args) {
+    //     DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+    //      System.out.println(myFormatObj);
+
+
+//     }
+// }
+ 
+
+
+
+
+
+// package college;
+// public class Book {
+//     public String name;
+//     public int price;
+
+//     public Book(String title, int mrp) {
+//         this.name = title;
+//         this.price = mrp;
+//     }
+// }
+
+ 
+//     public static void main(String[] args) {
+//         Book book1=new Book("My Frist Batch ", 100);
+//         System.out.println(book1.name+":"+book1.price);
+//         Book book2 =new Book("My Secand Batch ",  150);
+//         System.out.println(book2.name+":"+book2.price);
+//     }// pramiterse consitctor..
+// }
+
+
+// package college;
+// public class Book {
+//     public String name;
+//     public int price;
+
+//     public Book(String title, int mrp) {
+//         this.name = title;
+//         this.price = mrp;
+//     }
+// }
+
+ 
+//     public static void main(String[] args) {
+//         Book book1=new Book("My Frist Batch ", 100);
+//         System.out.println(book1.name+":"+book1.price);
+//         Book book2 =new Book("My Secand Batch ",  150);
+//         System.out.println(book2.name+":"+book2.price);
+//     }// pramiterse consitctor..
+// }
+
+// package college;
+
+// public class Student {
+//     public String name;
+//     public int age;
+//     public int rollNo;
+
+//     public Student(String name, int age, int rollNo) {
+//         this.name = name;
+//         this.age = age;
+//         this.rollNo = rollNo;
+//     }
+// }
+// 
